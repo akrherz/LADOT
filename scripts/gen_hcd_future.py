75894bd0187b72ef922410065e558895f0ed5068
akrherz <akrherz@iastate.edu>
2015-07-01 10:04:40 -0500

    more iterative improvements to HCD generator

--- scripts/gen_hcd_future.py
+++ scripts/gen_hcd_future.py
@@ -61,7 +61,6 @@ PERIODS = [E, G, A, C, D, B, F, H]
 # Precipitation Periods, Best Case
 PERIODS.reverse()
 
-
 # OFFSET these into the future period 2010->2050
 for i in range(len(PERIODS)):
     for j in range(2):
@@ -178,7 +177,7 @@ def runner():
             continue
         gridx, gridy = computeIJ(lon, lat)
 
-        print stid, tokens[1], gridx, gridy
+        print("%s %s X:%s Y:%s" % (stid, tokens[1], gridx, gridy,))
 
         s_atmpk = atmpk[:, gridy, gridx]
         s_asmps = asmps[:, gridy, gridx]
@@ -268,67 +267,69 @@ def runner():
                           psun[i], phour[i], relh[i]]
             now += interval
         # Now we need to do some smoothing between the periods to prevent
-        # wild jumps in the data
-        for (sts, ets) in PERIODS:
-            # We can't worry about the first year
-            if sts.year == 2010:
-                continue
-            sts += datetime.timedelta(hours=7)
-            t1 = sts + datetime.timedelta(hours=-1)
-            deltat = (ds[sts][0] - ds[t1][0]) / 2.
-            midpt = ds[sts][0] - deltat
-            rdelta = midpt - ds[sts][0]
-            ldelta = midpt - ds[t1][0]
-            oldt = []
-            newt = []
-            deltas = []
-            for hr in range(-24, 24):
-                ts = sts + datetime.timedelta(hours=hr)
-                delta = ldelta if hr < 0 else rdelta
-                newv = ((ds[ts][0] + delta * (1. / (abs(hr)+1)))
-                        if hr != 0 else midpt)
+        # wild jumps in the data, due to oddities with leap day, we need to
+        # merge both 1 January and 31 December, tricky!
+        for hroffset in [0, -24]:
+            for year in range(2011, 2050):
+                sts = datetime.datetime(year, 1, 1, 7)
+                sts += datetime.timedelta(hours=hroffset)
+                t1 = sts + datetime.timedelta(hours=-1)
+                deltat = (ds[sts][0] - ds[t1][0]) / 2.
+                midpt = ds[sts][0] - deltat
+                rdelta = midpt - ds[sts][0]
+                ldelta = midpt - ds[t1][0]
+                oldt = []
+                newt = []
+                deltas = []
+                for hr in range(-36, 36):
+                    ts = sts + datetime.timedelta(hours=hr)
+                    delta = ldelta if hr < 0 else rdelta
+                    newv = ((ds[ts][0] + delta * (1. / (abs(hr)+1)))
+                            if hr != 0 else midpt)
+                    if DEBUG:
+                        oldt.append(ds[ts][0])
+                        newt.append(newv)
+                        deltas.append(newt[-1] - ds[ts][0])
+                        print(("%s %5.2f [%5.2f] %5.2f %5.2f %5.2f %5.2f"
+                               ) % (ts.strftime("%Y%m%d%H"), oldt[-1],
+                                    newt[-1],
+                                    ds[ts][1],
+                                    ds[ts][2], ds[ts][3], ds[ts][4]))
+                    # Assign the new value
+                    ds[ts][0] = newv
+                    # print("%s -> %s %s" % (ts, newv, ds[ts][0]))
                 if DEBUG:
-                    oldt.append(ds[ts][0])
-                    newt.append(newv)
-                    deltas.append(newt[-1] - ds[ts][0])
-                    print(("%s %5.2f [%5.2f] %5.2f %5.2f %5.2f %5.2f"
-                           ) % (ts.strftime("%Y%m%d%H"), oldt[-1],
-                                newt[-1],
-                                ds[ts][1],
-                                ds[ts][2], ds[ts][3], ds[ts][4]))
-                # Assign the new value
-                ds[ts][0] = newv
-            if DEBUG:
-                (fig, ax) = plt.subplots(2, 1, sharex=True)
-                ax[0].set_title("Example Temp Smoother to Merge Periods")
-                ax[0].set_ylabel("Air Temperature $^\circ$F")
-                ax[0].bar(np.arange(-24, 24)-0.4, oldt, width=0.8,
-                          facecolor='tan',
-                          label='Old', zorder=1, edgecolor='tan')
-                ax[0].scatter(range(-24, 24), newt, marker='o', color='b',
-                              s=40, label='New', zorder=2)
-                ax[0].set_xlim(-25, 25)
-                ax[0].set_xticks(np.arange(-24, 24, 6))
-                ax[0].grid(True)
-                ax[0].set_ylim(min(oldt)-3, max(oldt)+3)
-                ax[0].legend(loc='best')
-                bars = ax[1].bar(np.arange(-24, 24)-0.4, deltas, width=0.8,
-                                 facecolor='b', edgecolor='b')
-                for i, bar in enumerate(bars):
-                    if deltas[i] > 0:
-                        bar.set_facecolor('r')
-                        bar.set_edgecolor('r')
-                ax[1].grid(True)
-                ax[1].set_ylabel("Delta Applied $^\circ$F")
-                ax[1].set_xlabel("Hours around 1 January 12 AM")
-                fig.savefig('test-%s.png' % (sts.strftime("%Y%m%d"),))
-                plt.close()
-                print("--------------")
+                    (fig, ax) = plt.subplots(2, 1, sharex=True)
+                    ax[0].set_title("Example Temp Smoother to Merge Periods")
+                    ax[0].set_ylabel("Air Temperature $^\circ$F")
+                    ax[0].bar(np.arange(-36, 36)-0.4, oldt, width=0.8,
+                              facecolor='tan',
+                              label='Old', zorder=1, edgecolor='tan')
+                    ax[0].scatter(range(-36, 36), newt, marker='o', color='b',
+                                  s=40, label='New', zorder=2)
+                    ax[0].set_xlim(-36, 36)
+                    ax[0].set_xticks(np.arange(-36, 36, 6))
+                    ax[0].grid(True)
+                    ax[0].set_ylim(min(oldt)-3, max(oldt)+3)
+                    ax[0].legend(loc='best')
+                    bars = ax[1].bar(np.arange(-36, 36)-0.4, deltas, width=0.8,
+                                     facecolor='b', edgecolor='b')
+                    for i, bar in enumerate(bars):
+                        if deltas[i] > 0:
+                            bar.set_facecolor('r')
+                            bar.set_edgecolor('r')
+                    ax[1].grid(True)
+                    ax[1].set_ylabel("Delta Applied $^\circ$F")
+                    ax[1].set_xlabel("Hours around 1 January 12 AM")
+                    fig.savefig('test-%s.png' % (sts.strftime("%Y%m%d"),))
+                    plt.close()
+                    print("--------------")
         # Now the magic to rewite these data
         out = open("%s.hcd" % (stid,), 'w')
         realts = datetime.datetime(2010, 1, 1)
         for (sts, ets) in PERIODS:
             now = sts
+            print 'now is %s realts is %s' % (now, realts)
             while now <= ets:
                 s = fmt % tuple(ds[now])
                 if s.find('nan') > 0:
@@ -339,7 +340,7 @@ def runner():
                 realts += datetime.timedelta(hours=1)
         out.close()
         subprocess.call("python qc_hcd.py %s.hcd" % (stid,), shell=True)
-        return
+        # return
 
 if __name__ == '__main__':
     runner()
